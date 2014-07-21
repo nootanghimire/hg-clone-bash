e17e3fd1323d320f2b3930c47dd1fd73a9ec5304
Nootan Ghimire <nootan.ghimire@gmail.com>
2014-07-21 12:33:43 +0545

    Update pull.sh
    not sure if this works

--- pull.sh
+++ pull.sh
@@ -9,3 +9,18 @@ while [ $i -lt 195225 ]
       i=$[$i+100]
       echo "$i" > config-a-bash-b
   done
+
+if [ $i -ge 195225 ];
+  then
+    rm -rf config-a-bash-b
+    echo "removed config-a-bash-b"
+    echo "Now running hg pull to make sure we have latest pulls"
+    hg pull
+    echo "Now running hg update to get a working copy"
+    hg update
+    echo ""
+    echo "Everything done! Plese delete pull.sh from your repo's root as you won't need it now"
+fi
+    
+    
+    
