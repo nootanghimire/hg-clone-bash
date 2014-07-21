ba1b0d9dc568b8fa04ab63ccf01180af1e37fbf9
Nootan Ghimire <nootan.ghimire@gmail.com>
2014-07-21 12:29:48 +0545

    Create clone.sh

---
+++ clone.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+hg clone --rev 100 $1 $2 
+cp pull.sh $2/pull.sh
+cd $2
+chmod +x pull.sh
+touch config-a-bash-b
+echo "100">config-a-bash-b
+echo "Cloning completed. Now running pull.sh Please run pull.sh in the repo's directory"
+
